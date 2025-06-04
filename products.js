68cfd3ea822abcc59af360846b7d0aa99efc72c4
KarthigaP20 <karthigaparthiban17@gmail.com>
2025-06-04 10:26:53 +0530

    Updated all Files

---
+++ products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+        id: 1,
+        name: "Three-Pack Linen Shirts",
+        src: "products/c1.jpg",
+        desc: "",
+        price: 220,
+        tags: ["new", "white", "summer"]
+    },
+    {
+        id: 2,
+        name: "Denim tie hem shirt",
+        src: "products/c2.jpg",
+        desc: "",
+        price: 360,
+        tags: ["new", "blue", "beach"]
+    },
+    {
+        id: 3,
+        name: "Khaki Utility Jacket",
+        src: "products/c3.jpg",
+        desc: "",
+        price: 399,
+        tags: ["old", "green", "party"]
+    },
+    {
+        id: 4,
+        name: "Olive Palazzo Pants",
+        src: "products/c4.jpg",
+        desc: "",
+        price: 399,
+        tags: ["old", "green", "summer"]
+    },
+    {
+        id: 5,
+        name: "Vanilla Flare Top",
+        src: "products/c5.jpg",
+        desc: "",
+        price: 579,
+        tags: ["old", "white", "beach"]
+    },
+    {
+        id: 6,
+        name: "Rust Harmony Set",
+        src: "products/c6.jpg",
+        desc: "",
+        price: 500,
+        tags: ["old", "red", "party"]
+    },
+    {
+        id: 7,
+        name: "Golden Co-ord Set",
+        src: "products/c7.jpg",
+        desc: "",
+        price: 399,
+        tags: ["old", "yellow", "party"]
+    },
+    {
+        id: 8,
+        name: "Army Casual Set",
+        src: "products/c8.jpg",
+        desc: "",
+        price: 499,
+        tags: ["new", "green", "party"]
+    },
+    {
+        id: 9,
+        name: "Vintage Skirt Look",
+        src: "products/c9.jpg",
+        desc: "",
+        price: 220,
+        tags: ["new", "green", "summer"]
+    }
+];
+
+export { products };
